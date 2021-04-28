826e75d0e2e960657d577823b79c2de786f1a005
juan.galarraga <juan_dlp2@outlook.com>
2021-04-28 16:29:41 -0300

    Buscaminas. Logica de matriz

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<title>Document</title>
+</head>
+<body>
+	<div id="root">
+		<div class="container">
+			<div class="row mt-2">
+				<div class="form-inline">
+					<label for="">Tamaño</label>
+					<select v-model="tamaño">
+						<option value="">5 x 5</option>
+						<option value="">10 x 10</option>
+						<option value="">5 x 5</option>
+					</select>
+				</div>
+				<buscaminas-wrapper :alto="10" :ancho="10"></buscaminas-wrapper>
+			</div>
+		</div>
+		<pre>
+			{{ $data }}			
+		</pre>
+	</div>
+<script src="js/vue.js"></script>
+
+<script type="text/x-template" id="buscaminas-wrapper">
+	<div class="d-flex">
+		<table>
+			<tbody>
+				<tr v-for="(numero, index) in fila">
+					<td class="bloque pointer" v-for="i in numero">{{ i }}</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</script>
+
+<script type="text/x-template" id="buscaminas-header">
+	<div class="form-inline">
+
+	</div>
+</script>
+
+<script src="js/app.js"></script>
+</body>
+</html>
--- js/app.js
+++ js/app.js
@@ -1,26 +1,45 @@
-const vue = new Vue({
-	el: '#root',
-	created (){
-		fetch(this.url)
-		.then(response => response.json())
-		.then(response => this.users = response.results.map(usuario => usuario.name));
+const buscaminasWrapper = {
+	template: '#buscaminas-wrapper',
+	props: {
+		ancho: Number,
+		alto: Number 
 	},
-	data: {
-		check: false,
-		url : 'https://randomuser.me/api/?results=23',
-		cant: 30,
-		check_userlist: false,
-		users: [],
-	},
-	computed:{
-		completeName(){
-            return this.users.map(usuario => `${usuario.first} ${usuario.last}`);
+	data(){
+		let fila = new Array();
+		let aux = 0;
+		
+		for (var i = 0; i < this.alto; i++) {
+			let column = new Array();
+			for (var j = 0; j < this.ancho; j++) {
+				let randomBit = this.randomInteger(0, 1);
+				column[j] = randomBit;
+			}
+			fila[i] = column;
+		}
+
+		console.log(fila);
+
+		return {
+			fila,
 		}
 	},
-	filters: {
-		ToUpperCase(data){
-			return data.toUpperCase();
+
+	methods: {
+		randomInteger(min, max) {
+			return Math.floor(Math.random() * (max - min + 1)) + min;
 		}
 	}
+}
 
+const vue = new Vue({
+	el: '#root',
+	data: {
+		options: {
+			alto: 5,
+			ancho: 5
+		}
+	},
+	components: {
+		buscaminasWrapper
+	},
 });
